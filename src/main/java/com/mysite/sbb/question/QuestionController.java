3d55e1fb3b6b12ab743f5daa195d4a421197bf0a
djunnni <djunnni@gmail.com>
2022-06-15 18:47:00 +0900

    NotFoundError 예시 추가, RequestMapping 부모로 변경

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,11 +3,13 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -18,12 +20,24 @@ public class QuestionController {
         롬복의 @Getter, @Setter가 자동으로 Getter, Setter 메서드를 생성하는 것과 마찬가지로 @RequiredArgsConstructor는 자동으로 생성자를 생성한다.
         따라서 스프링 의존성 주입 규칙에 의해 questionRepository 객체가 자동으로 주입된다.
      */
-    private final QuestionRepository questionRepository;
+    /*
+        스프링의 의존성 주입(Dependency Injection) 방식 3가지
+        @Autowired 속성 - 속성에 @Autowired 애너테이션을 적용하여 객체를 주입하는 방식
+        생성자 - 생성자를 작성하여 객체를 주입하는 방식 (권장하는 방식)
+        Setter - Setter 메서드를 작성하여 객체를 주입하는 방식 (메서드에 @Autowired 애너테이션 적용이 필요하다.)
+     */
+    private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
---
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    // questionRepository 객체는 생성자 방식으로 DI 규칙에 의해 주입된다.
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = this.questionRepository.findById(id);
+        if(oq.isPresent()) {
+            return oq.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
