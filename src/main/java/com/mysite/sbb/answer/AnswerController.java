7cdff62878f1cb23b54b5c5d9c260a7bc960472f
djunnni <djunnni@gmail.com>
2022-06-16 12:44:48 +0900

    valid 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -5,11 +5,14 @@ import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 
 @RequestMapping("/answer")
@@ -20,9 +23,15 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping(value="/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam("content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm,
+                               BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        answerService.create(question, content);
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 
